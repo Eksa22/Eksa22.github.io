95124b69d89fd0ba0418e2c9174488d1458a23d4
Eksa22 <54797651+Eksa22@users.noreply.github.com>
2019-12-10 22:12:23 +0700

    Add files via upload

---
+++ forum.js
@@ -0,0 +1,8 @@
+function kirim() {
+	var a=document.getElementById("abc").innerHTML;
+	var b=document.getElementById("teks").value;
+	var c=document.getElementById("kepala").value;
+	document.write(a);
+	document.write(b + "<br />" );
+	document.write(c+ "<br />"+"<hr/>");
+}
